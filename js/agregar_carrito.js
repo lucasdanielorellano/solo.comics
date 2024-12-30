ea5d3bcc1b5d4d1f7846c9d532f689be6bd585c8
lucasdanielorellano <gis.gonzalez@bue.edu.ar>
2024-12-30 14:48:49 -0300

    se agregó colecciones y se ajustaron botones

--- js/agregar_carrito.js
+++ js/agregar_carrito.js
@@ -1,25 +1,7 @@
-// cuando se carga el dom se ejecuta el codigo 
-/*
-productoA = document.getElementById("productoA");
-productoB = document.getElementById("productoB");
-productoC = document.getElementById("productoC");
-
-productoA.addEventListener("click", () => {
-    // AGREGAR EL LOCALSTORAGE
-    alert("Producto A agregado al carrito");
-});
-
-productoB.addEventListener("click", () => {
-    alert("Producto B agregado al carrito");
-});
-
-productoC.addEventListener("click", () => {
-    alert("Producto C agregado al carrito");
-});*/
 document.addEventListener("DOMContentLoaded", () => {
     // guardo en una constante, un array de etiquetas 
     // es un nodeList del tipo element, adentro guarda todas las anclas que encuentra que tengan la clase btn-agregar
-    const botonesAgregar = document.querySelectorAll(".btn-agregar");
+    const botonesAgregar = document.querySelectorAll(".btn-primary");
     console.log(botonesAgregar);
     
     // recorrreme la lista de nodos (array)
